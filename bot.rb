3611bb239fed2438507dee050cceec64b7d6aa44
Fizal Sarif <fizal.sarif@gmail.com>
2017-08-28 21:11:20 -0400

    organized message better

--- bot.rb
+++ bot.rb
@@ -1,7 +1,11 @@
+# slack.chat_postMessage(channel: '#questions_test', text: 'Hello World')
 require 'slack-ruby-client'
 require 'pp'
-# slack.chat_postMessage(channel: '#questions_test', text: 'Hello World')
+
+listen_on = 'ta_talk'
 reminderTimes = {}
+
+
 
 Slack.configure do |config|
   config.token = ENV['SLACK_API_TOKEN']
@@ -16,15 +20,17 @@ end
 
 
 client.on 'user_typing' do |data|
+  user = slack.users_info(user: data.user).user
+  message="Hey #{user.real_name}! \nI see you're typing in the #questions channel. \nI just wanted to remind you of the guidelines for effectively asking for help in an engineering setting. \n Make sure to be as clear as possible, the easier it is to understand the situation you're in - the faster someone in there can help you fix your issue. You can read more about how to clearly commuicate your programming issues here: https://stackoverflow.com/help/how-to-ask. \n\nThe recommended way to ask a question here at NYCDA would be to follow this template (feel free to copy paste): \n\n*What's wrong:* \n `(A clear description of the issue)` \n*This is the error message I'm getting:* \n `(Copy paste the error you got. Make sure to use three backticks to make it easier to read)` \n*I tried to:* \n `(A short description of what you tried to solve this problem. Including any resources you found online wouuld be great too!)` \n\nStructuring your question like this would let me know I should ping the TAs :smile:"
+
   if reminderTimes[data.user]
     # 3000*24=72000 seconds in a day
     if (Time.now.to_i - reminderTimes[data.user].to_i) > 69000
-      client.message channel: "<@#{data.user}>", text: 'is this a dm?'
+      slack.chat_postMessage channel: "@"+user.name, text: message, as_user: false
     end
   else
     reminderTimes[data.user] = Time.now
-    user = slack.users_info(user: data.user).user
-    slack.chat_postMessage channel: "@"+user.name, text: "Hey #{user.real_name}! \nI see you're typing in the #questions channel. \nI just wanted to remind you of the guidelines for effectively asking for help in an engineering setting. \n Make sure to be as clear as possible, the easier it is to understand the situation you're in - the faster someone in there can help you fix your issue. You can read more about how to clearly commuicate your programming issues here: https://stackoverflow.com/help/how-to-ask. \n\nThe recommended way to ask a question here at NYCDA would be to follow this template (feel free to copy paste): \n\n*What's wrong:* \n `(A clear description of the issue)` \n*This is the error message I'm getting:* \n `(Copy paste the error you got. Make sure to use three backticks to make it easier to read)` \n*I tried to:* \n `(A short description of what you tried to solve this problem. Including any resources you found online wouuld be great too!)` \n\nStructuring your question like this would let me know I should ping the TAs :smile:", as_user: false
+    slack.chat_postMessage channel: "@"+user.name, text: message, as_user: false
   end
 end
 
